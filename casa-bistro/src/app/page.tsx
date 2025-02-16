bba760c4e76d25f15737e6e3994747dcc968a336
adeboyega <8955yomi@gmail.com>
2025-02-16 03:20:10 +0100

    web icon update

--- casa-bistro/src/app/page.tsx
+++ casa-bistro/src/app/page.tsx
@@ -4,13 +4,18 @@ import React from "react";
 import Image from "next/image";
 // import { Dinner } from "./Myicons/icons";
 // Define interfaces for a menu item and menu category
-
+import Head from "next/head";
 
 import { menuData } from "./MenuData";
  
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+   <>
+   <Head>
+    <link rel="icon" type="image/png" href="/casalogo.png" className="imageDark" />
+    <link rel="icon" type="image/png" href="/casalogo.png" className="imageLight" />
+   </Head>
+   <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
       <div
         className="fixed inset-0 z-0 imageDark"
@@ -100,5 +105,6 @@ export default function Home() {
         );
       })}
     </div>
+   </>
   );
 }
